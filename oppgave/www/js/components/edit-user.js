341f611f1ef7e9b64504981d2403e63eeb53d2cc
Brautaset <petterjb@stud.ntnu.no>
2020-12-17 12:32:38 +0100

    Besvarelse

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -1,3 +1,4 @@
+//edit-user.js
 import { LitElement, html, css } from "../../node_modules/lit-element/lit-element.js";
 
 class EditUser extends LitElement {
@@ -7,7 +8,56 @@ class EditUser extends LitElement {
     };
   }
 
-  // din kode her
+  //Render html by JS. Contains a user form with mailadresse (uid), fornavn, etternavn, gammelt passord
+  //and nytt passord. The page contains a form where the user can edit their corresponding user data.
+  render() {
+    return html`
+    <head>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    </head>
+    <form onsubmit="javascript: return false;" id="userForm" method="POST">
+    <div class="form-group pt-3 ml-5" style="width: 30rem;">
+      <label for="email">Mailadresse:</label>
+      <input class="form-control" id="uname" name="uname" type="text" value="${this.user.uname}" required>
+      <input type="hidden" id="uid" name="uid" value="${this.user.uid}">
+    </div>
+    <div class="form-group pt-1 ml-5" style="width: 30rem;">
+      <label for="firstName">Fornavn:</label>
+      <input class="form-control" id="firstName" name="firstName" type="text" value="${this.user.firstName}" required>
+    </div>
+    <div class="form-group pt-1 ml-5" style="width: 30rem;">
+      <label for="lastName">Etternavn:</label>
+      <input class="form-control" id="lastName" name="lastName" type="text" value="${this.user.lastName}" required>
+    </div>
+    <div class="form-group pt-1 ml-5" style="width: 30rem;">
+      <label for="oldpwd">Gammelt passord:</label>
+      <input type="password" class="form-control" id="oldpwd" name="oldpwd" type="text" value="">
+    </div>
+    <div class="form-group pt-1 ml-5" style="width: 30rem;">
+      <label for="newpwd">Nytt passord:</label>
+      <input type="password" class="form-control" id="pwd" name="pwd" type="text" value="">
+  </div>
+  <input type="submit" @click=${this.updateUser} id="submitForm" name="editUser" class="btn btn-info mt-4 ml-2" value="Edit User"></input>
+</form>
+    `;
+  }
 
+  //Updating data on user
+  updateUser(err) {
+    //Data fetched from the HTML-file
+    const dataForm = new FormData(e.target.form);
+    console.log(err)
+    fetch('api/updateUser.php', {
+     method: 'POST',
+     body: dataForm
+    }).then(res=>res.json())
+      .then(data=>{
+        if (data.status=='Success') {
+            console.log("User data was updated");
+        } else {
+            console.log("The user data was not updated");
+        }
+      })
+  }
 }
 customElements.define('edit-user', EditUser);
--- readme.md
+++ readme.md
@@ -1,4 +1,6 @@
-# PROG2053 eksamen v2020
+StudentID: 10109
+
+#PROG2053 eksamen v2020
 * Lag en FORK av dette repositoriet og gi meg leserettigheter (aland.a.mendoza@ntnu.no). Det som er i repositoriet på slutten av eksamen teller som din besvarelse.
 * Legg inn studentnummeret ditt øverst i readme.md fila.
 * IKKE kjør "npm install" i www mappa i oppgave, se siste kommentar i oppgaven.
@@ -11,3 +13,5 @@ Lag en side som lister ut alle brukere. Hent brukere fra api/fetchUsers.php med 
 I denne oppgaven skal du lage samme funksjonaliteten med web komponenter. I filen oppgave3.html finner du startpunktet for denne oppgaven. Denne filen bruker users-list.js komponenten i js/components mappen til å lage en liste av alle brukere, funksjonaliteten i denne komponenten er ferdig laget. I render (på linje 56) brukes en edit-user.js komponent, denne komponenten får tilsendt den valgte brukeren. Det som mangler i edit-user.js er funksjonaliteten for å oppdatere en bruker.
 
 NB, i oppgave/www/node_modules ligger lit-element med de endringer som skal til for at det fungerer uten å bruke "polymer serve" (dvs, koden fungerer direkte i docker/XAMPP).
+
+SVAR: The answer is in oppgave3.html,, uses fetch-user it uses www/js/edit-user.js to edit an user
